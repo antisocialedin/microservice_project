dc8da68d5d31cdbaeec876bb1ed5f50a0bb8e295
Eder Filho <ederlopes_7@hotmail.com>
2023-03-28 13:51:02 -0300

    Revert "remove front-end"
    This reverts commit 7866567bd8c50a6f0f196d39c067bd5a2143bd03.

---
+++ react-front/src/users/EditUser.js
@@ -0,0 +1,101 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
+export default function EditUser() {
+  let navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const [user, setUser] = useState({
+    nome: "",
+    idade: "",
+    telefone: "",
+  });
+
+  const { nome, idade, telefone} = user;
+
+
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.put(`http://localhost:8080/user/${id}`, user);
+    navigate("/");
+  };
+
+  const loadUser = async () => {
+    const result = await axios.get(`http://localhost:8080/user/${id}`);
+    setUser(result.data);
+  };
+
+  return (
+    <div className="container">
+        <div className="row">
+            <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+                <h2 className="text-center m-4">Editar Usuário</h2>
+
+                <form onSubmit={(e) => onSubmit(e)}>
+                    
+                    <div className="mb-3">
+                        <label htmlFor="Nome" className="form-label">
+                            Nome
+                        </label>
+                        <input
+                            type={"text"}
+                            className="form-control"
+                            placeholder="Digite seu nome"
+                            name="nome"
+                            value={nome}
+                            onChange={(e) => onInputChange(e)}
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="Idade" className="form-label">
+                            Idade
+                        </label>
+                        <input
+                            type={"text"}
+                            className="form-control"
+                            placeholder="Ex: 21"
+                            name="idade"
+                            value={idade}
+                            onChange={(e) => onInputChange(e)}
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="Telefone" className="form-label">
+                            Telefone
+                        </label>
+                        <input
+                            type={"text"}
+                            className="form-control"
+                            placeholder="Ex:(11)91234-5678"
+                            name="telefone"
+                            value={telefone}
+                            onChange={(e) => onInputChange(e)}
+                        />
+                    </div>
+
+                    <button type="submit" className="btn btn-outline-primary">
+                        Confirmar
+                    </button>
+                    
+                    <Link className="btn btn-outline-danger mx-2" to="/">
+                        Cancelar
+                    </Link>
+                                    
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ react-front/src/users/ViewUser.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+export default function ViewUser() {
+
+    const { id } = useParams();
+
+    const [user, setUser] = useState({
+        nome: "",
+        idade: "",
+        telefone: "",
+    })
+
+    const { nome, idade, telefone} = user;
+    
+  useEffect(() => {
+    loadUser()
+  }, [])
+
+  const loadUser = async () => {
+    const result = await axios.get(`http://localhost:8080/user/${id}`);
+    setUser(result.data);
+  };
+
+  return (
+    <div className="container">
+        <div className="row">
+            <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+                <h2 className="text-center m-4">Detalhes do Usuário</h2>
+                    
+                <div className="card">
+                    <div className="card-header">
+                     Detalhes do usuário id : {user.id}
+                        <ul className="list-group list-group-flush">
+                        
+                            <li className="list-group-item">
+                                <b>Nome:</b>
+                                {user.nome}
+                            </li>
+
+                            <li className="list-group-item">
+                                <b>Idade:</b>
+                                {user.idade}
+                            </li>
+
+                            <li className="list-group-item">
+                                <b>Telefone:</b>
+                                {user.telefone}
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+
+                <Link className="btn btn-primary my-2" to={"/"}>
+                    Voltar
+                </Link>
+
+            </div>
+        </div>
+    </div>
+  );
+}
